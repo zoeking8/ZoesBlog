c3db0f4820cf87afacb54481b960af740d4788f0
Zoe King <zoeking@live.co.uk>
2019-10-18 15:21:23 +0100

    clear identity changes

---
+++ ZoesBlog/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using ZoesBlog.Areas.Identity.Data;
+using ZoesBlog.Models;
+
+[assembly: HostingStartup(typeof(ZoesBlog.Areas.Identity.IdentityHostingStartup))]
+namespace ZoesBlog.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+                services.AddDbContext<ZoesBlogContext>(options =>
+                    options.UseSqlServer(
+                        context.Configuration.GetConnectionString("ZoesBlogContextConnection")));
+
+                services.AddDefaultIdentity<User>(options => options.SignIn.RequireConfirmedAccount = true)
+                    .AddEntityFrameworkStores<ZoesBlogContext>();
+            });
+        }
+    }
+}
