28ce3cc8fb4d98f4c0f8b1f6ac36e40d4bed43b5
Zoe King <zoeking@live.co.uk>
2019-10-31 13:59:25 +0000

    TagCloud working

--- ZoesBlog/Pages/TagCloud.cshtml.cs
+++ ZoesBlog/Pages/TagCloud.cshtml.cs
@@ -1,61 +1,116 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Linq;
-using System.Threading.Tasks;
+using System.Text;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
-using Sparc.TagCloud;
 using ZoesBlog.Data;
-
+using Microsoft.AspNetCore.Html;
 namespace ZoesBlog.Pages
 {
-    public class TagCloudModel : PageModel
+	public class TagCloudModel : PageModel
     {
+		public HtmlString Tags { get; private set; }
 		private readonly BlogDbContext _blogDbContext;
 
-		public Tag Tag { get; private set; }
-		public string UrlSlug { get; set; }
-		public List<BlogPost> BlogPosts { get; set; }
-		public List<TagCloudTag> TagCloud { get; private set; }
+
 		public TagCloudModel(BlogDbContext blogDbContext)
 		{
 			_blogDbContext = blogDbContext;
 		}
-
-		
-		private List<TagCloudTag> GenerateTagCloud()
-		{
-			var analyzer = new TagCloudAnalyzer();
-
-			var blogPostTags = _blogDbContext
-							   .Tags
-							   .Select(t => t.Name)
-							   .ToList();
-
-
-			var tags = analyzer.ComputeTagCloud(blogPostTags);
-
-			tags = tags.Shuffle();
-			return tags.ToList();
-		}
 		public IActionResult OnGet()
 		{
-			TagCloud = GenerateTagCloud();
+			var CloudTags = _blogDbContext.Tags;
+
+			var groupedTags = (from t in CloudTags
+						group t by t.Name into g
+						select new { Name = g.Key, Count = g.Count() });
+			groupedTags = (from t in groupedTags
+						   orderby t.Name descending
+						   select t);
+
+			double minSize = 10;
+			double maxSize = 100;
+
+			double steps = (maxSize - minSize) / groupedTags.Count();
+
+			StringBuilder sb = new StringBuilder();
+			foreach (var tag in groupedTags)
+			{
+				double size = minSize + ((double)tag.Count - 1) * steps;
+				sb.Append("<span style='font-size:" + size + "pt'>" + tag.Name + "(" + tag.Count + ") </span>");
+			}
+			var sbTags = sb.ToString();
+			Tags = new HtmlString(sbTags);
 			return Page();
 
 		}
 
-		//public void OnGet()
-		//      {
-		//	var analyzer = new TagCloudAnalyzer();
 
-		//	// blogPosts is an IEnumerable<String>, loaded from
-		//	// the database or whatevz.
-		//	var tags = analyzer.ComputeTagCloud(blogPosts);
 
-		//	// Shuffle the tags, if you like for a random
-		//	// display
-		//	tags = tags.Shuffle();
-		//}
-	}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+	//private readonly BlogDbContext _blogDbContext;
+
+	//public Tag Tag { get; private set; }
+	//public string UrlSlug { get; set; }
+	//public List<BlogPost> BlogPosts { get; set; }
+	//public List<TagCloudTag> TagCloud { get; private set; }
+	//public TagCloudModel(BlogDbContext blogDbContext)
+	//{
+	//	_blogDbContext = blogDbContext;
+	//}
+
+
+	//private List<TagCloudTag> GenerateTagCloud()
+	//{
+	//	var analyzer = new TagCloudAnalyzer();
+
+	//	var blogPostTags = _blogDbContext
+	//					   .Tags
+	//					   .Select(t => t.Name)
+	//					   .ToList();
+
+
+	//	var tags = analyzer.ComputeTagCloud(blogPostTags);
+
+	//	tags = tags.Shuffle();
+	//	return tags.ToList();
+	//}
+	//public IActionResult OnGet()
+	//{
+	//	TagCloud = GenerateTagCloud();
+	//	return Page();
+
+	//}
+
+	//public void OnGet()
+	//      {
+	//	var analyzer = new TagCloudAnalyzer();
+
+	//	// blogPosts is an IEnumerable<String>, loaded from
+	//	// the database or whatevz.
+	//	var tags = analyzer.ComputeTagCloud(blogPosts);
+
+	//	// Shuffle the tags, if you like for a random
+	//	// display
+	//	tags = tags.Shuffle();
+	//}
+}
 }
